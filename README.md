70b89bbc12863d5492567e5bfefe50e38fcfd502
cmoulliard <cmoulliard@redhat.com>
2025-04-10 07:40:13 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## IDP Packages
+
+- [external-secrets](external-secrets) - https://external-secrets.io/latest/introduction/getting-started/
---
+++ external-secrets/generate-manifests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+cd "$(dirname "$0")"
+
+if [[ -n $EXTERNAL_SECRETS_CHART_VERSION ]]; then
+  CHART_VERSION=$EXTERNAL_SECRETS_CHART_VERSION
+else
+  CHART_VERSION="0.15.1"
+fi
+
+INSTALL_YAML="manifests/install.yaml"
+
+echo "# EXTERNAL SECRETS INSTALL RESOURCES" >${INSTALL_YAML}
+echo "# This file is auto-generated with 'ref-impelmentation/external-secrets/generate-manifests.sh'" >>${INSTALL_YAML}
+
+helm repo add external-secrets --force-update https://charts.external-secrets.io
+helm repo update
+helm template --namespace external-secrets external-secrets external-secrets/external-secrets -f values.yaml --version ${CHART_VERSION} >>${INSTALL_YAML}
